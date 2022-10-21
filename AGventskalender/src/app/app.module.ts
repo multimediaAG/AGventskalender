6a83f7b1f7bbb6ed99fcd4013016197c956f110e
haager-ca <caius.haager@icloud.com>
2022-10-21 20:21:17 +0200

    added pages

--- AGventskalender/src/app/app.module.ts
+++ AGventskalender/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { HomeComponent } from './_pages/home/home.component';
+import { RegisterComponent } from './_pages/register/register.component';
+import { LoginComponent } from './_pages/login/login.component';
+import { RulesComponent } from './_pages/rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
